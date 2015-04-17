7b2724fcbe346c130c3017558b6371d5a210d7c6
Andy Walker <walkeraj@gmail.com>
2015-04-17 15:45:39 -0400

    adding an example viralnova only seemed fitting

---
+++ examples/viralnova/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	cb "github.com/alaska/fishorcutclickbait"
+	"os"
+	"strings"
+)
+
+func main() {
+	doc, err := goquery.NewDocument(`http://viralnova.com`)
+	if err != nil {
+		fmt.Printf("Attempt to pull from %s failed: [%s]\n", "buzzfeed", err.Error())
+		os.Exit(1)
+	}
+	total := 0
+	clickbait := 0
+	doc.Find(`h4.snip > a`).Each(func(i int, s *goquery.Selection) {
+		total++
+		title := strings.TrimSpace(s.Text())
+		if cb.IsClickbait(title) {
+			fmt.Println(title, "identified as clickbait")
+			clickbait++
+		}
+	})
+	fmt.Printf("%d/%d articles identified as clickbait\n", clickbait, total)
+}
